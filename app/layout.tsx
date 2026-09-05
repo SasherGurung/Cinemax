b454f38bd8980d14d891840faf84fb6873e347f5
Sasher Gurung <sashergrg602@gmail.com>
2026-09-05 22:57:00 +0545

    feat: added types for discover movie

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "react-hot-toast";
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -21,8 +16,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.className} h-full antialiased`}
     >
+      <Toaster position="top-right" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
